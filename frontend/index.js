1aefa1271cacd0b2aa1a3280f7b28ba913033833
FF0r3x <99687709+FF0r3x@users.noreply.github.com>
2022-02-18 22:29:01 +0100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('https://vast-retreat-41648.herokuapp.com/');
+const socket = io('https://dazzling-brahmagupta-49df4e.netlify.app/');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
