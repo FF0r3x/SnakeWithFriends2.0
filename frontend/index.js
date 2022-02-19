7a92896d758978e427adc96d0978daf868e6fa83
FF0r3x <99687709+FF0r3x@users.noreply.github.com>
2022-02-19 11:53:37 +0100

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('https://LightgreenActualAxis.gydoyt.repl.co');
+const socket = io('https://SnakeWithFriends.gydoyt.repl.co');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
